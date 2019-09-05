c434614e62424e8d4c5941cf8851dd063bcd22f7
Landry Irakoze <irakozel@gmail.com>
2019-09-05 13:25:33 -0400

    config files

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-const server = require('./api/server.js');
+require('dotenv').config();
 
-const port = 4000;
-server.listen(port, () => {
-  console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
+const server = require('./api/server.js');
+const defaults = require('./config/defaults.js');
+
+// const port = 4000;
+// server.listen(port, () => {
+//   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
+// });
+
+server.listen(defaults.port, () => {
+  console.log(`\n*** Server Running on http://localhost:${defaults.port} ***\n`);
 });
---
+++ notes.md
@@ -0,0 +1,19 @@
+# Deployment
+
+## Make Port Dynamic
+
+### Set up COntinuous Deployment on Heroku
+
+make changes > commit and push > get deployed
+
+### Steps
+
+- create new app on heroku, App needs a unique name.
+- get to the "Deploy" tab
+- click on Github button and consent to the security prompt
+- search for your repository on github
+- click "Connect" next to the repository name
+- choose the branch to deploy and click "Enable Automatic Deploys"
+- click "Deploy Branch"
+- go to the "Overview" tab
+- once it is deployed click on "Open App" on the top right
